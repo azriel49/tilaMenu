12c497dfe1deb1f9100b94f71caca0ed1ef3d822
AzrieL <nicolas@azriel.org>
2015-09-02 23:12:47 +0200

    Add:  - Class on links
    Fix:
     - heading's ids mess...

--- jquery.tilaMenu.js
+++ jquery.tilaMenu.js
@@ -3,7 +3,8 @@
  *
  * Copyright (c) 2015 Nicolas Matheu
  *
- * Licensed under the GNU GENERAL PUBLIC LICENSE Version 2
+ * Licensed under the GPL license:
+ *   http://opensource.org/licenses/gpl-3.0.html
  *
  */
 (function($) {
@@ -22,10 +23,12 @@
             idPrefix: 'tilaMenu',
             menu: '#tilaMenu',
             title: 'h2',
+            anchorClassname: 'tilaMenu-anchor',
             beforeItem: '<li>',
             afterItem: '</li>'
         };
         var config=$.extend(defauts, options);
+        /**************************/
         return this.each(function(){
             $(this).find(config.title).each(function(){
                 var h2content = $(this).html();
@@ -33,8 +36,9 @@
                     h2content=h2content.replace(':','');
                 }
                 var h2id = Math.floor((Math.random() * 999999) + 100000);
-                $(this).attr('id','autoH2-'+h2id);
-                $(config.menu).append(config.beforeItem+'<a href="#'+config.idPrefix+'-'+h2id+'" title="'+h2content+'">'+h2content+'</a>'+config.afterItem);
+                $(this).attr('id',config.idPrefix+'-'+h2id);
+                $(this).attr('name',config.idPrefix+'-'+h2id);
+                $(config.menu).append(config.beforeItem+'<a href="#'+config.idPrefix+'-'+h2id+'" title="'+h2content+'" class="'+config.anchorClassname+'">'+h2content+'</a>'+config.afterItem);
             });
         });
     };
